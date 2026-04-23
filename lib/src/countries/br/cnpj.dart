e4a85497d4edad8fc2c4cbf2647c92bb09a52005
Luiz Santos <56003422+augustodia@users.noreply.github.com>
2026-04-23 18:25:56 -0300

    ✨ Addes support for the new Brazilian alphanumeric CNPJ check digit calculation (#1)
    Co-authored-by: Luiz Augusto <luiz.santos@tbdc.com.br>

--- lib/src/countries/br/cnpj.dart
+++ lib/src/countries/br/cnpj.dart
@@ -1,31 +1,57 @@
 import 'package:stdnum_dart/src/exceptions.dart';
-import 'package:stdnum_dart/src/helpers/utils.dart';
 import 'package:stdnum_dart/src/interfaces/document_interface.dart';
 
 class BR_CNPJ implements DocumentInterface {
-  int _computeDigit(List<int> digits, List<int> factors) {
-    final modulus = 11;
+  static final RegExp _validCNPJ = RegExp(r'^[A-Z0-9]{12}\d{2}$');
+  static const List<int> _firstDigitWeights = [
+    5,
+    4,
+    3,
+    2,
+    9,
+    8,
+    7,
+    6,
+    5,
+    4,
+    3,
+    2,
+  ];
+  static const List<int> _secondDigitWeights = [
+    6,
+    5,
+    4,
+    3,
+    2,
+    9,
+    8,
+    7,
+    6,
+    5,
+    4,
+    3,
+    2,
+  ];
 
-    final digit =
-        factors
-            .asMap()
-            .entries
-            .map((entry) => digits[entry.key] * entry.value)
-            .reduce((a, b) => a + b) %
-        modulus;
+  int _characterValue(String character) {
+    return character.codeUnitAt(0) - 48;
+  }
 
-    return digit <= 1 ? 0 : 11 - digit;
+  int _computeDigit(String value, List<int> weights) {
+    final sum = weights
+        .asMap()
+        .entries
+        .map((entry) => _characterValue(value[entry.key]) * entry.value)
+        .reduce((a, b) => a + b);
+    final remainder = sum % 11;
+
+    return remainder <= 1 ? 0 : 11 - remainder;
   }
 
   ({int firstDigit, int secondDigit}) _computeDigits(String cnpj) {
-    final digits = cnpj.split('').map((d) => int.parse(d)).toList();
-    final factors = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-
-    final firstDigit = _computeDigit(
-      digits,
-      factors.sublist(1, factors.length),
-    );
-    final secondDigit = _computeDigit(digits, factors);
+    final body = cnpj.substring(0, 12);
+    final firstDigit = _computeDigit(body, _firstDigitWeights);
+    final secondDigit = _computeDigit('$body$firstDigit', _secondDigitWeights);
 
     return (firstDigit: firstDigit, secondDigit: secondDigit);
   }
@@ -40,21 +66,24 @@ class BR_CNPJ implements DocumentInterface {
 
   @override
   String compact(String cnpj) {
-    return cnpj.replaceAll(RegExp(r'[-./]'), '');
+    return cnpj.replaceAll(RegExp(r'[-./]'), '').toUpperCase();
   }
 
   @override
   String format(String cnpj) {
     final clearedCNPJ = compact(cnpj);
+    if (clearedCNPJ.length != 14) {
+      throw InvalidLength();
+    }
+
     return '${clearedCNPJ.substring(0, 2)}.${clearedCNPJ.substring(2, 5)}.${clearedCNPJ.substring(5, 8)}/${clearedCNPJ.substring(8, 12)}-${clearedCNPJ.substring(12, 14)}';
   }
 
   @override
   String validate(String cnpj) {
     final clearedCNPJ = compact(cnpj);
-    final isOnlyDigits = StdnumDartUtils.isOnlyDigits(clearedCNPJ);
-    if (!isOnlyDigits) throw InvalidFormat();
     if (clearedCNPJ.length != 14) throw InvalidLength();
+    if (!_validCNPJ.hasMatch(clearedCNPJ)) throw InvalidFormat();
     final originalDigits = _extractDigits(clearedCNPJ);
     final calculedDigits = _computeDigits(clearedCNPJ);
     if (calculedDigits.firstDigit != originalDigits.firstDigit ||
--- test/src/br/cnpj_test.dart
+++ test/src/br/cnpj_test.dart
@@ -2,28 +2,86 @@ import 'package:stdnum_dart/src/countries/br/cnpj.dart';
 import 'package:stdnum_dart/src/exceptions.dart';
 import 'package:test/test.dart';
 
+String _calculateCheckDigit(String value, List<int> weights) {
+  final sum = weights
+      .asMap()
+      .entries
+      .map((entry) => (value[entry.key].codeUnitAt(0) - 48) * entry.value)
+      .reduce((a, b) => a + b);
+  final remainder = sum % 11;
+
+  return '${remainder <= 1 ? 0 : 11 - remainder}';
+}
+
+String _buildCNPJ(String body) {
+  final firstDigit = _calculateCheckDigit(body, [
+    5,
+    4,
+    3,
+    2,
+    9,
+    8,
+    7,
+    6,
+    5,
+    4,
+    3,
+    2,
+  ]);
+  final secondDigit = _calculateCheckDigit('$body$firstDigit', [
+    6,
+    5,
+    4,
+    3,
+    2,
+    9,
+    8,
+    7,
+    6,
+    5,
+    4,
+    3,
+    2,
+  ]);
+
+  return '$body$firstDigit$secondDigit';
+}
+
+String _formatCNPJ(String cnpj) {
+  return '${cnpj.substring(0, 2)}.${cnpj.substring(2, 5)}.${cnpj.substring(5, 8)}/${cnpj.substring(8, 12)}-${cnpj.substring(12, 14)}';
+}
+
+final generatedAlphanumericCNPJ = _buildCNPJ('ABCDEF123456');
+
 final validCNPJs = [
   '27.964.657/0001-25',
   '85.060.965/0001-22',
   '20.060.005/0001-17',
+  '12.ABC.345/01DE-35',
+  '12.abc.345/01de-35',
+  _formatCNPJ(generatedAlphanumericCNPJ),
 ];
 final invalidCNPJs = [
   '27.964.657/0001-00',
   '27.964.657/0001-10',
   '85.060.965/0001-12',
   '20.060.005/0001-77',
+  '12.ABC.345/01DE-00',
+  _formatCNPJ('${generatedAlphanumericCNPJ.substring(0, 12)}00'),
 ];
 final invalidLengthCNPJs = [
   '12.456.78',
   '12.456.789',
   '12.456.789/0000',
   '12.456.789/00000',
+  '12.ABC.345/01DE-3',
+  '12.ABC.345/01DE-355',
 ];
 final invalidFormatCNPJs = [
-  '12.A56.789/0198-55',
-  'AA.BBB.CCC/DDHD-56',
-  '12.456.789/01AB-23',
-  '12.456.789/02A1-11',
+  '12.ABC.345/01DE-3A',
+  '12.ABC.345/01D_-35',
+  '12.ABC.345/01DÉ-35',
+  '12.ABC.345/01DE-AA',
 ];
 void main() {
   for (var cnpj in validCNPJs) {
@@ -53,4 +111,14 @@ void main() {
       expect(isValid, throwsA(isA<InvalidFormat>()));
     });
   }
+
+  test('cnpj compact alphanumeric', () async {
+    final compacted = BR_CNPJ().compact('12.abc.345/01de-35');
+    expect(compacted, '12ABC34501DE35');
+  });
+
+  test('cnpj format alphanumeric', () async {
+    final formatted = BR_CNPJ().format('12abc34501de35');
+    expect(formatted, '12.ABC.345/01DE-35');
+  });
 }
